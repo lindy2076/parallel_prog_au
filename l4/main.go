021dc3b350c5352e146336b9191e7123a75819c7
lindy2076 <kakaporuqee@gmail.com>
2024-04-16 23:31:05 +0300

    l4/e divide estimation into smaller chunks

--- l4/main.go
+++ l4/main.go
@@ -115,14 +115,21 @@ func mainE() {
 	fmt.Printf("%d ITERATIONS ARE ENOUGH FOR THE DESIRED ERROR\n", nE)
 	fmt.Printf("%g IS THE RESULT\n", kindaE(nE))
 
+	fmt.Printf("%d GOROUTINES ARE READY TO CALCULATE\n", threads)
+	for k := 1; k < threads+1; k++ {
+		t1 := time.Now().UnixMicro()
+		parallelE(nE, k)
+		t2 := time.Now().UnixMicro()
+		fmt.Printf("%d THREADS CALCULATED IN %d MICROS\n", k, t2-t1)
+	}
 }
 
 func nForGiverErr_E(uErr float64) int {
 	n := 1
 	prevRes := kindaE(n)
 	for n = 2; math.Abs(prevRes-math.E) > uErr; n++ {
-		fmt.Printf("%g\n", prevRes)
-		prevRes *= calcNthTerm_E(n)
+		// prevRes *= calcNthTerm_E(n)
+		prevRes *= calcKthTerm_E(n)
 	}
 	return n - 1
 }
@@ -130,11 +137,13 @@ func nForGiverErr_E(uErr float64) int {
 func kindaE(n int) float64 {
 	res := 2.0
 	for i := 1; i < n+1; i++ {
-		res *= calcNthTerm_E(i)
+		// res *= calcNthTerm_E(i)
+		res *= calcKthTerm_E(i)
 	}
 	return res
 }
 
+// original function to estimate. N is for each root
 func calcNthTerm_E(n int) float64 {
 	res := 1.0
 	for i := 0; i < intPow(2, n-1); i++ {
@@ -144,6 +153,26 @@ func calcNthTerm_E(n int) float64 {
 	return res
 }
 
+// calcNthTerm split into terms.
+func calcKthTerm_E(k int) float64 {
+	ceil2n, n := CeilToPowerOf2(k)
+	max := ceil2n * 2
+	maxAmount := intPow(2, n-1)
+	d := max - 2*(maxAmount) + 2*(ceil2n-k)
+
+	return math.Pow(float64(d)/float64(d-1), 1/float64(ceil2n))
+}
+
+// Ceil to the nearest power of 2 that is bigger than num.
+func CeilToPowerOf2(num int) (int, int) {
+	n := 0
+	for i := 1; i <= num; i = i << 1 {
+		n += 1
+	}
+	return intPow(2, n), n
+}
+
+// Returns x to the power of n.
 func intPow(x, n int) int {
 	if n == 1 {
 		return x
@@ -152,4 +181,29 @@ func intPow(x, n int) int {
 		return 1
 	}
 	return x * intPow(x, n-1)
+}
+
+// Estimate e with *threads* goroutines and n iterations of kindaE
+func parallelE(n, threads int) float64 {
+	var wg sync.WaitGroup
+	wg.Add(threads)
+
+	piRes := &ResWithLock{res: 2.0}
+
+	for thread := 1; thread < threads+1; thread++ {
+		go func(tNum int) {
+			defer wg.Done()
+
+			threadRes := 1.0
+			for ns := tNum; ns < n; ns += threads {
+				// threadRes *= calcNthTerm_E(ns)
+				threadRes *= calcKthTerm_E(ns)
+			}
+			piRes.mu.Lock()
+			defer piRes.mu.Unlock()
+			piRes.res *= threadRes
+		}(thread)
+	}
+	wg.Wait()
+	return piRes.res
 }
